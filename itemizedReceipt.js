e52c5e84971ebf4ebed5c25bbee8644bdec55f76
vasilevisovan <vasile.visovan@yahoo.com>
2023-10-31 20:48:56 -0700

    final

--- itemizedReceipt.js
+++ itemizedReceipt.js
@@ -17,13 +17,13 @@ const obj1 = {description : 'Kebab', price : 17.99 }
 const obj2 = {description : 'Shawarma', price : 19.99 }
 const obj3 = {description : 'Fish', price : 12.99 }
 const obj4 = {description : 'Chips', price : 7.99 }
-
+console.log(logReceipt(obj1, obj2, obj3, obj4));
 // Check
-logReceipt(
+/*logReceipt(
   { descr: 'Burrito', price: 5.99 },
   { descr: 'Chips & Salsa', price: 2.99 },
   { descr: 'Sprite', price: 1.99 }
-);
+);*/
 // should log something like:
 // Burrito - $5.99
 // Chips & Salsa - $2.99
--- spaceShip.js
+++ spaceShip.js
@@ -2,8 +2,29 @@
 // - should set two properties: name and topSpeed
 // - should have a method accelerate that logs to the console 
 //   `${name} moving to ${topSpeed}`
-
-
-
+class SpaceShip {
+    constructor(name, topSpeed) {
+      this.name = name;
+      this.topSpeed = topSpeed;
+    }
+  
+    accelerate() {
+      console.log(`${this.name} accelerating to ${this.topSpeed}`);
+    }
+  }
+  
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
+  const spaceship1 = new SpaceShip("Zmeu", "Viteza sunetului(speed of sound) ");/*The Zmeu is a fantastic creature of 
+  Romanian folklore and Romanian mythology. Though referred by some sources as a dragon, 
+  the zmeu is nevertheless distinct, because it usually has clear anthropomorphic  traits: it is humanoid and has legs, arms,
+   the ability to create and use artifacts such as weapons, and to ride a horse, and has the desire 
+  to marry young girls. There are commentators that class it as a giant (equivalent of an ogre), or a devil, 
+  or even a vampire.*/
+ 
+  const spaceship2 = new SpaceShip("Balaur", "Viteza luminii(speed of light)");/*A balaur in Romanian folklore is a type of 
+  many-headed dragon or monstrous serpent, sometimes said to be equipped with wings. The number of heads is usually around three, 
+  but they can also have seven heads or even twelve heads according to some legends.*/
+  
+  spaceship1.accelerate();
+  spaceship2.accelerate();
